4670ca6bd4a7393e34959c7a5a61a93fc23349ff
Jan <info@phoca.cz>
2019-02-10 15:16:58 +0100

    3.1.7

--- mod_phocacart_product.php
+++ mod_phocacart_product.php
@@ -7,14 +7,18 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  */
  
-defined('_JEXEC') or die('Restricted access');// no direct access
+defined('_JEXEC') or die;// no direct access
 
 if (!JComponentHelper::isEnabled('com_phocacart', true)) {
 	$app = JFactory::getApplication();
 	$app->enqueueMessage(JText::_('Phoca Cart Error'), JText::_('Phoca Cart is not installed on your system'), 'error');
 	return;
 }
-if (! class_exists('PhocaCartLoader')) {
+
+JLoader::registerPrefix('Phocacart', JPATH_ADMINISTRATOR . '/components/com_phocacart/libraries/phocacart');
+
+/*
+if (! class_exists('PhocacartLoader')) {
     require_once( JPATH_ADMINISTRATOR.'/components/com_phocacart/libraries/loader.php');
 }
 
@@ -25,7 +29,8 @@ phocacartimport('phocacart.path.route');
 phocacartimport('phocacart.price.price');
 phocacartimport('phocacart.currency.currency');
 phocacartimport('phocacart.product.product');
-phocacartimport('phocacart.image.image');
+phocacartimport('phocacart.image.image');*/
+
 
 
 $lang = JFactory::getLanguage();
@@ -33,12 +38,25 @@ $lang = JFactory::getLanguage();
 $lang->load('com_phocacart');
 JHTML::stylesheet('media/com_phocacart/css/main.css' );
 
-$p['item_ordering']	= $params->get( 'item_ordering', 1 );
-$p['item_limit']	= $params->get( 'item_limit', 1 );
+$p['item_ordering']					= $params->get( 'item_ordering', 1 );
+$p['item_limit']					= $params->get( 'item_limit', 1 );
+$p['hide_price']					= $params->get( 'hide_price', 0 );
+$p['display_view_product_button']	= $params->get( 'display_view_product_button', 1 );
+$p['catid_multiple']				= $params->get( 'catid_multiple', array() );
+$p['featured_only']					= $params->get( 'featured_only', 0 );
+//$p['stock_checking']				= $params->get( 'stock_checking', 0 );// in module display all products
 
-// TODO - the following function can check publish, stock, price - this can be added to the parameters
-$products			= PhocaCartProduct::getProducts($p['item_limit'], $p['item_ordering']);
-$t['pathitem'] 		= PhocaCartpath::getPath('productimage');
+$p['display_product_description']	= $params->get( 'display_product_description', 0 );
+$p['module_description']			= $params->get( 'module_description', '' );
+
+$moduleclass_sfx 					= htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8');
+
+$pCom								= JComponentHelper::getParams( 'com_phocacart' );
+$pc['display_star_rating']			= $pCom->get( 'display_star_rating', 0 );
+
+// TO DO - the following function can check publish, stock, price - this can be added to the parameters
+$products			= PhocacartProduct::getProducts(0, $p['item_limit'], $p['item_ordering'], 0, true, false, false, 0, $p['catid_multiple'], $p['featured_only']);
+$t['pathitem'] 		= PhocacartPath::getPath('productimage');
 
 
 require(JModuleHelper::getLayoutPath('mod_phocacart_product'));
--- tmpl/default.php
+++ tmpl/default.php
@@ -6,19 +6,26 @@
  * @copyright Copyright (C) Jan Pavelka www.phoca.cz
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  */
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-$layoutV	= new JLayoutFile('button_product_view', $basePath = JPATH_ROOT .'/components/com_phocacart/layouts');
-$layoutP	= new JLayoutFile('product_price', $basePath = JPATH_ROOT .'/components/com_phocacart/layouts');
+$layoutV	= new JLayoutFile('button_product_view', null, array('component' => 'com_phocacart'));
+$layoutP	= new JLayoutFile('product_price', null, array('component' => 'com_phocacart'));
+$layoutR	= new JLayoutFile('product_rating', null, array('component' => 'com_phocacart'));
 
-echo '<div class="ph-product-module-box">';
+echo '<div class="ph-product-module-box'.$moduleclass_sfx .'">';
+
+if ($p['module_description'] != '') {
+	echo '<div class="ph-mod-desc">'.$p['module_description'].'</div>';
+}
 if (!empty($products)) {
 	foreach ($products as $k => $v) {
 		
+		echo '<div class="ph-item-box">';
+		
 		echo '<div class="thumbnail ph-thumbnail">';
 
-		$image = PhocaCartImage::getThumbnailName($t['pathitem'], $v->image, 'medium');
-		$link = JRoute::_(PhocaCartRoute::getItemRoute($v->id, $v->catid, $v->alias, $v->categoryalias));
+		$image = PhocacartImage::getThumbnailName($t['pathitem'], $v->image, 'medium');
+		$link = JRoute::_(PhocacartRoute::getItemRoute($v->id, $v->catid, $v->alias, $v->catalias));
 		echo '<a href="'.$link.'">';
 		if (isset($image->rel) && $image->rel != '') {
 			echo '<img src="'.JURI::base(true).'/'.$image->rel.'" alt="" class="img-responsive ph-image"';
@@ -31,40 +38,70 @@ if (!empty($products)) {
 		echo '<div class="caption">';
 		echo '<h3>'.$v->title.'</h3>';
 		
+		
+		// REVIEW - STAR RATING
+		if ((int)$pc['display_star_rating'] > 0) {
+			$d							= array();
+			$d['rating']				= isset($v->rating) && (int)$v->rating > 0 ? (int)$v->rating : 0;
+			$d['size']					= 16;
+			$d['display_star_rating']	= (int)$pc['display_star_rating'];
+			echo $layoutR->render($d);
+		}
+		
 		// Description box will be displayed even no description is set - to set height and have all columns same height
 		echo '<div class="ph-item-desc">';
-		if ($v->description != '') {
+		if ($v->description != '' && (int)$p['display_product_description'] > 0) {
 			echo $v->description;
 		}
 		echo '</div>';// end desc
 		
 		// :L: PRICE
-		$price 				= new PhocaCartPrice;
-		$d					= array();
-		$d['priceitems']	= $price->getPriceItems($v->price, $v->taxrate, $v->taxcalculationtype, $v->taxtitle, $v->unit_amount, $v->unit_unit);
-		$d['priceitemsorig']= array();
-		if ($v->price_original != '' && $v->price_original > 0) {
-			$d['priceitemsorig'] = $price->getPriceItems($v->price_original, $v->taxrate, $v->taxcalculationtype);
+		if ($p['hide_price'] != 1) {
+			$price 				= new PhocacartPrice;
+			$d					= array();
+			$d['priceitems']	= $price->getPriceItems($v->price, $v->taxid, $v->taxrate, $v->taxcalculationtype, $v->taxtitle, $v->unit_amount, $v->unit_unit, 1, 1, $v->group_price);
+			$d['priceitemsorig']= array();
+			if ($v->price_original != '' && $v->price_original > 0) {
+				$d['priceitemsorig'] = $price->getPriceItems($v->price_original, $v->taxid, $v->taxrate, $v->taxcalculationtype);
+			}
+			$d['class']			= 'ph-category-price-box';// we need the same class as category or items view
+			$d['product_id']	= (int)$v->id;
+			$d['typeview']		= 'Module';
+			
+			// Display discount price
+			// Move standard prices to new variable (product price -> product discount)
+			$d['priceitemsdiscount']		= $d['priceitems'];
+			$d['discount'] 					= PhocacartDiscountProduct::getProductDiscountPrice($v->id, $d['priceitemsdiscount']);
+			
+			// Display cart discount (global discount) in product views - under specific conditions only
+			// Move product discount prices to new variable (product price -> product discount -> product discount cart)
+			$d['priceitemsdiscountcart']	= $d['priceitemsdiscount'];
+			$d['discountcart']				= PhocacartDiscountCart::getCartDiscountPriceForProduct($v->id, $v->catid, $d['priceitemsdiscountcart']);
+			
+			$d['zero_price']		= 1;// Apply zero price if possible
+			echo $layoutP->render($d);
 		}
-		$d['class']			= 'ph-category-price-box';
-		echo $layoutP->render($d);
 		
 		// VIEW PRODUCT BUTTON
 		echo '<div class="ph-category-add-to-cart-box">';
 		
 		// :L: LINK TO PRODUCT VIEW
-		$d					= array();
-		$d['link']			= $link;
-		echo $layoutV->render($d);
+		if ((int)$p['display_view_product_button'] > 0) {
+			$d									= array();
+			$d['link']							= $link;
+			$d['display_view_product_button'] 	= $p['display_view_product_button'];
+			echo $layoutV->render($d);
+		}
 	
 		
 		echo '</div>';// end add to cart box
 		echo '<div class="clearfix"></div>';
-		
+
 		
 		echo '</div>';// end caption
 		
 		echo '</div>';// end thumbnail
+		echo '</div>';// end ph-item-box
 	}
 }
 echo '</div>';
